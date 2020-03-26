c444f9189645c511046aa7a94c9b75e1d0461388
gomasiokomachi <yuno.thevamps@gmail.com>
2020-03-26 17:45:54 +0900

    fontawesome追加等footer修正

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 import '../css/app.css'
 import swiper from './swiper.js'
 swiper()
+
+import {config, dom, library} from '@fortawesome/fontawesome-svg-core'
+import {} from '@fortawesome/free-solid-svg-icons'
+import {} from '@fortawesome/free-regular-svg-icons'
+import {faTwitterSquare, faInstagramSquare, faFacebookSquare} from '@fortawesome/free-brands-svg-icons'
+
+library.add(faTwitterSquare,faInstagramSquare,faFacebookSquare)
+
+dom.i2svg()
